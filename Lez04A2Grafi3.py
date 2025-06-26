ef3e49374a354d974952008491569ddde043156c
rick <riccardo.lupo2611@gmail.com>
2025-06-26 19:11:53 +0200

    esercizio Lez04A2Grafi3 trovato su github corretto

--- Lez04A2Grafi3.py
+++ Lez04A2Grafi3.py
@@ -1,21 +1,18 @@
-def calcola_percorso(P,x,y):
-    n = 0
-    while n < len(P):
-        n +=1
-        if P[x] == y:
-            return True,n
-        if P[x] == P[y]:
-            return True,n+1
-        x = P[x]
-    return False, n
+def calcola_percorso(P, n, nodi_percorsi):
+    while n != P[n]:
+        nodi_percorsi[n] += 1
+        n = P[n]
+    nodi_percorsi[n] += 1
 
-def calcola_distanza(P,u,v):
-     UV_check,UV_dist= calcola_percorso(P,u,v)
-     VU_check,VU_dist= calcola_percorso(P,v,u)
-     if not UV_check and not VU_check:
-        return "nessun percorso trovato"
-     if UV_check: return UV_dist
-     return VU_dist
+def calcola_distanza(P, u, v):
+    nodi_percorsi = [0] * len(P)
+    calcola_percorso(P, u, nodi_percorsi)
+    calcola_percorso(P, v, nodi_percorsi)
+    distanza = 0
+    for i in range(len(nodi_percorsi)):
+        if nodi_percorsi[i] == 1:
+            distanza += 1
+    return distanza
 
 P = [1, 1, 0, 1, 0, 2, 2, 4]
-print(calcola_distanza(P,3,4))
+print(calcola_distanza(P, 7, 6))
