43e216c350b5a58e117df333b3cd26b9dc46625b
rick <riccardo.lupo2611@gmail.com>
2025-07-02 21:04:02 +0200

    soluzione esercizio 4

--- Esercizio4ProgrDinamica.py
+++ Esercizio4ProgrDinamica.py
@@ -1,18 +1,25 @@
 def ex4(M):
     n = len(M[0])
     T = [[0 for k in range(n)] for _ in range(n)]
-    T[0] = M[0]
-    for x in range(n):
-        for y in range(n):
-            T[x][0] = M[x][0]
-
-
+    matrice_max = 0
     for i in range(n):
         for j in range(n):
-            if
+            if M[i][j] == 1:
+                if i == 0 or j == 0:
+                    T[i][j] = M[i][j]
+                else:
+                    T[i][j] = 1+ min(T[i-1][j],T[i][j-1], T[i-1][j-1])
+            if T[i][j] > matrice_max:
+                matrice_max = T[i][j]
+    return matrice_max
+
+
+
 
 M = [[1,0,1,1,1],
      [1,1,1,1,1],
      [1,1,1,0,1],
      [1,1,1,1,1],
      [1,1,0,1,1]]
+
+print(ex4(M))
