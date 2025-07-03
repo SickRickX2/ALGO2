3cb7a2258240a281436b453ba5084d8b3d5edab5
rick <riccardo.lupo2611@gmail.com>
2025-07-03 12:22:32 +0200

    soluzione es2 foglio Back Tracking

---
+++ es2BackT.py
@@ -0,0 +1,24 @@
+def es2(n, sol = [], ca = 0,cb = 0):
+    if n%2 == 1: return
+    if len(sol) == n:
+        print("".join(sol))
+        return
+    if len(sol)+2 <= n:
+        sol.append("a")
+        es2(n,sol,ca+1,cb)
+        sol.pop()
+        sol.append("b")
+        es2(n,sol,ca,cb+1)
+        sol.pop()
+    if (ca%2== 0 and cb %2 ==1):
+        sol.append("a")
+        es2(n,sol,ca+1,cb)
+        sol.pop()
+    if (cb %2==0 and cb%2 == 1):
+        sol.append("b")
+        es2(n,sol,ca,cb+1)
+        sol.pop()
+
+
+
+es2(4)
